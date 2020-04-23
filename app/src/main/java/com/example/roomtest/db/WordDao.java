b0f7da0be14e2e5dfc12506a046c8799f5eb2515
dyegral <1357442171@qq.com>
2020-04-23 11:30:20 +0800

    first commit

---
+++ app/src/main/java/com/example/roomtest/db/WordDao.java
@@ -0,0 +1,28 @@
+package com.example.roomtest.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+    @Insert
+    void insertWords(Word... words);
+
+    @Update
+    void updateWords(Word... words);
+
+    @Delete
+    void deleteWords(Word... words);
+
+    @Query("DELETE FROM WORD")
+    void deleteAll();
+
+    @Query("SELECT * FROM WORD ORDER BY ID DESC")
+    List<Word> getAllWords();
+
+}
